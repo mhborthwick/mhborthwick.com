2bdea35e8ced5038cc2d144b216d5a1a5f7dd92f
Mike Borthwick <mhborthwick@gmail.com>
2023-10-01 02:38:06 -0700

    sort newest to oldest

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,11 @@ import { PageContainer, ProjectGrid, ProjectGridItem } from "../components";
 import { projects } from "../assets/projects.json";
 
 export function Home() {
+  const reversed = [...projects].reverse();
   return (
     <PageContainer>
       <ProjectGrid>
-        {projects.map((p) => (
+        {reversed.map((p) => (
           <ProjectGridItem key={p.id} project={p} />
         ))}
       </ProjectGrid>
