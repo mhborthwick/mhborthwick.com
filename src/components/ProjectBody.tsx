cf7d2d81b3b40a34dd07d58ab95c857c58babeb1
Mike Borthwick <mhborthwick@gmail.com>
2023-10-06 00:07:17 -0700

    style body

--- src/components/ProjectBody.tsx
+++ src/components/ProjectBody.tsx
@@ -6,6 +6,8 @@ interface ProjectBodyProps {
 
 export function ProjectBody({ project }: ProjectBodyProps) {
   return (
-    <div className="max-w-[800px] m-auto my-24 mx-8">{project.description}</div>
+    <div className="max-w-[800px] m-auto my-20 sm:mx-8">
+      {project.description}
+    </div>
   );
 }
--- src/components/ProjectHeading.tsx
+++ src/components/ProjectHeading.tsx
@@ -7,13 +7,15 @@ interface ProjectHeadingProps {
 
 export function ProjectHeading({ project }: ProjectHeadingProps) {
   return (
-    <div className="flex items-start justify-start relative">
+    <div className="relative md:flex md:items-start md:justify-start">
       <motion.img
         className="w-full aspect-[4/3] object-cover"
         src={project.imgUrl}
       />
-      <div className="absolute text-white m-4">
-        <motion.h2 className="text-4xl font-bold">{project.title}</motion.h2>
+      <div className="my-2 md:absolute md:text-white md:m-4">
+        <motion.h2 className="text-4xl md:text-6xl font-bold">
+          {project.title}
+        </motion.h2>
         <motion.div className="text-sm">{project.subtitle}</motion.div>
       </div>
     </div>
