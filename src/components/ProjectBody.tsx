0c7c815706989756d2cec9999ecc78095887162e
Mike Borthwick <mhborthwick@gmail.com>
2023-10-07 17:38:18 -0700

    animate body

--- src/components/ProjectBody.tsx
+++ src/components/ProjectBody.tsx
@@ -1,13 +1,41 @@
+import { motion } from "framer-motion";
 import { Project } from "../interfaces";
 
 interface ProjectBodyProps {
   project: Project;
 }
 
+const descriptionVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.4,
+    },
+  },
+  exit: {
+    opacity: 0,
+    rotateX: 90,
+    transition: {
+      ease: "easeOut",
+      duration: 0.2,
+    },
+  },
+};
+
 export function ProjectBody({ project }: ProjectBodyProps) {
   return (
-    <div className="max-w-[800px] m-auto my-20 sm:mx-8">
+    <motion.div
+      variants={descriptionVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="max-w-[800px] m-auto my-20 sm:mx-8"
+    >
       {project.description}
-    </div>
+    </motion.div>
   );
 }
